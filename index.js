831fcc42042c6c0438eb08cf3d3b842055bd14de
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-06-17 21:26:16 +0300

    commit tree 79250ba8a27697301d22f4bdc8e2b7ebb09a9afc

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+function reverse(word) {
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = reverse(word);
+
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
+  that means if the word is the same as the word in reverse, I should return true.
+  
+  reverse the input string
+
+  if the input is the same as the reversed input
+    return true
+  else
+    return false
 */
 
 /*
@@ -20,6 +34,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome(""));
 }
 
 module.exports = isPalindrome;
